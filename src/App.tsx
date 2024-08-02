3d2de9035ca45127e9a894b7752910721f9c0086
Abakar26 <bku52135@gmail.com>
2024-08-02 16:07:06 +0500

    Add React app with TypeScript and Tailwind CSS

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function App() {
+  return (
+    <div className="text-3xl font-bold underline flex justify-center">
+      Invoice Assessment Form
+    </div>
+  );
+}
+
+export default App;

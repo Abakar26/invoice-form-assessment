06f04c3fdd122cf243a888be701c78c136f4a501
Abakar26 <bku52135@gmail.com>
2024-08-04 16:39:41 +0500

    feature/InvoiceForm implemented header and invoice form

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
-import React from "react";
+import Header from "./components/Header";
+import InvoiceFrom from "./components/invoiceForm";
 
 function App() {
   return (
-    <div className="text-3xl font-bold underline flex justify-center">
-      Invoice Assessment Form
-    </div>
+    <>
+      <Header />
+      <InvoiceFrom />
+    </>
   );
 }
 
